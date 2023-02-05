b619b09cefc4f71d4f0567a15ed122e111c45101
Evaggelos Soulintzis <esoulintzhs@gmail.com>
2023-02-06 00:46:47 +0200

    Add comments and revision black example

--- examples/black_example.py
+++ examples/black_example.py
@@ -1,56 +1,47 @@
+# """The following example demonstrate how we can use Black to better format our code"""
 # def calculate_factorial(number):
+#   """A function with wrong indentation (2 spaces instead of 4)"""
 #   if number == 0:
 #     return 1
 #   else:
 #     return (
-#         number * calculate_factorial(number - 1)
+#       number * calculate_factorial(number - 1)
 #     )
 
-
+# """That should have been in one line of code"""
 # num = int(
 #     input(
 #         "Enter a number: "
 #         )
 #     )
 
-
+# """The operator should had been surrounded with spaces"""
 # if num<0:
 #     print("Invalid input")
-# else:
-#     result = calculate_factorial(num)
-#     print(
-#         "The factorial of", num, "is", result)
 
 
-# def a_function_with_a_lot_of_arguments( name, default=None,*args,variable="1123",a='1', b = '2',c , another_variavle='value' ,an_array,test,ad,office,d,e,f,**kwargs):
-#     """This is function is created to demonstrate black"""
+# """A function with a lot of arguments that should be on seperate lines of code"""
+# def a_function_with_a_lot_of_arguments( name, default=None,*args,variable = "1123",a='1', b = '2',c , another_variavle='value' ,an_array,test,ad,office,d,e,f,**kwargs):
+#   pass
 
-
+# """Wrong formated list of numbers"""
 # j = [1,
 #  2, 3
 # ]
 
-
-# def is_unique(s):
-#     s = list(s)
-#     s.sort()
-
-#     for i in range(len(s) - 1):
-#         if s[i] == s[i + 1]:
-#             return 0
-#     else:
-#         return 1
-
+# def function_1():
+#   """Functions outside a class should be seperated by two lines"""
+#   pass
+# def function_2():
+#   """Also variables outside a class should be seperated by two lines"""
+#   pass
+# names = ['John', 'George']
 
 # class RandomClass:
-#     def init(self, a, b):
-#         self.a = a
-#         self.b = b
-
-
-#     def sum(self):
-#         return self.a+self.b
-
-# if __name__ == "__main__":
-#     print(
-#         is_unique(  input() ) )
+#   names = ['John', 'George']
+#   def function_1(self):
+#     """Functions inside a class should be seperated by one line"""
+#     pass
+#   def function_2(self):
+#     """Also variables inside a class should be seperated by one line"""
+#     pass
